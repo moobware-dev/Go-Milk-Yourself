7d04cfd21ccc8f3031b766539b7f6218a9d4e231
Moobware Dev <dev@moobware.com>
2018-06-10 20:05:54 -0500

    First pass of receiving mouse input for controlling the game

---
+++ Assets/Programming/Debug/DebugMouseInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugMouseInput : MonoBehaviour {
+
+    public Button MouseDown;
+
+    public Slider MousePositionLeftHalfOfScreen;
+    public Slider MousePositionRightHalfOfScreen;
+
+    public Color ButtonHeldColor;
+    public Color ButtonNeutralColor;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mouseOnLeftHalfOfScreen = Input.mousePosition.x <= (Screen.width / 2);
+        var mouseVerticalPosition = 
+            ((Input.mousePosition.y / Screen.height) - 0.5f) * 2;
+
+        MousePositionLeftHalfOfScreen.value = mouseOnLeftHalfOfScreen ? mouseVerticalPosition : 0;
+
+        MousePositionRightHalfOfScreen.value = !mouseOnLeftHalfOfScreen ? mouseVerticalPosition: 0;
+
+        var mouseDown = Input.GetMouseButton(0);
+
+        MouseDown.image.color = mouseDown ? ButtonHeldColor : ButtonNeutralColor;
+    }
+}
